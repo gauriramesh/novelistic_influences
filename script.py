75fdc1b1f5b909e8b3285567c6b54265cb65b56b
gauri <gauri.ramesh15@gmail.com>
2017-11-23 23:00:59 -0600

    Imported book file, broke up string into words, and removed stopwords

---
+++ script.py
@@ -0,0 +1,32 @@
+import gensim
+from nltk.tokenize import word_tokenize
+import string
+from nltk.corpus import stopwords
+class Extractor:
+
+    def __init__(self):
+        pass
+
+    def filegetter(self):
+        book = open("gaurisbook.txt", "r")
+        print "Name of file: ", book.name
+
+    def cleanText(self, dirtyWordList):
+        stopw = stopwords.words('english') + list(string.punctuation)
+        return [i for i in dirtyWordList if i not in stopw]
+
+
+
+
+if __name__ == '__main__':
+    print("something")
+    ext = Extractor()
+    ext.filegetter()
+    with open("gaurisbook.txt") as f:
+        words = [word for line in f for word in line.split()]
+    cleanWordList = ext.cleanText(words)
+    print(cleanWordList)
+
+
+
+
