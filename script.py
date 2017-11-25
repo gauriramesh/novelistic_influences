ef5fec97599cd6d711383eb9408c6b641250d04b
gauri <gauri.ramesh15@gmail.com>
2017-11-24 19:09:21 -0600

    Modified cleanup to accurately split along punctuation

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import gensim
 from nltk.tokenize import word_tokenize
 import string
 from nltk.corpus import stopwords
+import re
 class Extractor:
 
     def __init__(self):
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     ext = Extractor()
     ext.filegetter()
     with open("gaurisbook.txt") as f:
-        words = [word for line in f for word in line.split()]
+        words = [word for line in f for word in re.findall(r"[\w']+|[.,!?;]", line)]
     cleanWordList = ext.cleanText(words)
     print(cleanWordList)
 
